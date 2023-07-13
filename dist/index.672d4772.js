2764b034cac100db8518da5cc9ca42dca4abc182
IslaRalston <138975667+IslaRalston@users.noreply.github.com>
2023-07-13 10:54:11 +0000

    button working

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -1,6 +1,10 @@
 function showAns2() {
-    const element = document.getElementById("q2");
-    element.classList.remove("hidden");
+    const div = document.getElementById("q2");
+    div.classList.remove("hidden");
+    // const button = get the button by id isla
+    // button.classList. now isla, figure thsi one out
+    const button = document.getElementById("b2");
+    button.classList.add("hidden");
 }
 
 //# sourceMappingURL=index.672d4772.js.map
