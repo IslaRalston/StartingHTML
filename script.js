43054bccdb44b57c7f807d9f3ac4129b2fd7954a
IslaRalston <138975667+IslaRalston@users.noreply.github.com>
2023-07-14 10:09:29 +0000

    got other buttons working

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function showAns2() {
   const div = document.getElementById("q2");
   div.classList.remove("hidden");
-
-  // const button = get the button by id isla
-  // button.classList. now isla, figure thsi one out
   const button = document.getElementById("b2");
   button.classList.add("hidden");
 }
 window.showAns2 = showAns2;
+
+function showAns3() {
+  const div = document.getElementById("q3");
+  div.classList.remove("hidden");
+  const button = document.getElementById("b3");
+  button.classList.add("hidden");
+}
+window.showAns3 = showAns3;
+
+function showAns1() {
+  const div = document.getElementById("q1");
+  div.classList.remove("hidden");
+  const button = document.getElementById("b1");
+  button.classList.add("hidden");
+}
+window.showAns1 = showAns1;
