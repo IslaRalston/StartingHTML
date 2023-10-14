bda87efaf545fbb770ccf93e571904cb4ec002d5
IslaRalston <138975667+IslaRalston@users.noreply.github.com>
2023-10-14 11:48:11 +0000

    coffee

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-function showAns2() {
-  const div = document.getElementById("q2");
-  div.classList.remove("hidden");
-  const button = document.getElementById("b2");
-  button.classList.add("hidden");
+function answer(){
+  const box = document.getElementById('a');
+  const ansDiv= document.getElementById('ans');
+  const answer =box.value;
+  if(answer == 'coffee'){
+    ansDiv.innerHTML = 'correct';
+  }
+  
+  if (answer != 'coffee'){
+    ansDiv.innerHTML = 'wrong';
+  }
 }
-window.showAns2 = showAns2;
 
-function showAns3() {
-  const div = document.getElementById("q3");
-  div.classList.remove("hidden");
-  const button = document.getElementById("b3");
-  button.classList.add("hidden");
-}
-window.showAns3 = showAns3;
-
-function showAns1() {
-  const div = document.getElementById("q1");
-  div.classList.remove("hidden");
-  const button = document.getElementById("b1");
-  button.classList.add("hidden");
-}
-window.showAns1 = showAns1;
+window.answer=answer;
